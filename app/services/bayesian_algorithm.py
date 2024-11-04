8e03a14ec39d4234e2ef190e7ced6af6b47d25f7
ruan.souza <ruan.souza@ndd.tech>
2024-11-04 12:45:39 -0300

    ajuste

--- app/services/bayesian_algorithm.py
+++ app/services/bayesian_algorithm.py
@@ -6,19 +6,9 @@ class Algorithm:
   arquivo = 'app/data/dadosCod.csv'
   temp = 'Age,Sex,Job,Housing,Saving_accounts,Checking_account,Credit_amount,Duration,Purpose,Risk'
   
-  def define_bins(self, df, column_name, bins, labels):
-    """Classifica os dados de acordo com as faixas definidas."""
-    df[column_name] = pd.cut(df[column_name], bins=bins, labels=labels, right=False)
-    return df
-  
   def Calculate(self, dado):
     colunas_totais = self.temp.split(',')
     dados = pd.read_csv(self.arquivo, names=colunas_totais)
-    
-    # dados = self.define_bins(dados, 'Age', bins=[0, 18, 30, 40, 50, 60, 100], labels=['0', '1', '2', '3', '4', '5'])
-    # dados = self.define_bins(dados, 'Credit_amount', bins=[0, 1000, 5000, 10000, 15000], labels=['0', '1', '2', '3'])
-    
-    # dados.to_csv(self.arquivo, index=False, header=False)
     
     modelo =  BayesianNetwork([('Risk', 'Age'), 
                                ('Risk', 'Sex'), 
