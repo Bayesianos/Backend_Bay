0648768b1a29e8bce56bdd1e15f5e8203c204038
MAONEZZE <ruansanchezskt@gmail.com>
2025-12-07 14:14:28 -0300

    ajuste 2

--- app/main.py
+++ app/main.py
@@ -1,30 +1,20 @@
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 from fastapi.middleware.cors import CORSMiddleware
-from contextlib import asynccontextmanager
 import logging
 
 from app.routes.chat_route import router as chat_router
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger('    ')
-
-@asynccontextmanager
-async def inicio(app: FastAPI):
-  logger.info('Iniciando Aplicação...')
-  yield
-  logger.info('Encerrando Aplicação...')
   
-
 app = FastAPI( 
   title="Bayesian API",
   description="API para verificação de emprestimo com base em rede bayesiana",
   version="1.0.0",
-  lifespan=inicio
 )
 
 origins = [
-  "http://localhost:3000",
   "https://frontend-bay-uytb.onrender.com"
 ]
 
@@ -43,6 +33,11 @@ async def docs():
   logger.info('redirencionando para o swagger')
   return RedirectResponse(url="/docs")
 
-@app.get('/home', include_in_schema=False)
-async def home():
-  return {'pagina home': 'hello world'}
+@app.on_event("startup")
+async def iniciandoAPP():
+  logger.info("Iniciando Aplicação...")
+
+@app.on_event("shutdown")
+async def encerrandoAPP():
+  logger.info("Encerrando Aplicação...")
+
